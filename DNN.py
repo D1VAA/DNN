1c173b0d22575c7fe8656038b79fadda29ed1cae
HyBun <davipare203@gmail.com>
2023-07-02 01:21:19 -0300

    developing the backpropagation and changed the feedforward propagation method

--- DNN.py
+++ DNN.py
@@ -1,10 +1,9 @@
 import numpy as np
 from time import sleep
 from bcolors import Colors
-from learning import SGD
 
 
-class NeuralNetwork(SGD):
+class NeuralNetwork:
     def __init__(self):
         self.hidden_layers = None
         self.training_data = None
@@ -19,19 +18,42 @@ class NeuralNetwork(SGD):
         self.results = list()
 
     @staticmethod
-    # Method that do the basic perceptron operation
-    def __perceptron(x, w, b):
-        data = np.array(x)
-        weight = np.array(w)
-        bias = np.array(b)
-        result = (np.dot(data, weight) + bias)[0]
-        return result
-
-    @staticmethod
     def __activation_function(value: int, deriv=False) -> int or float:
         if deriv:
             return value * (1 - value)
         return 1 / (1 + np.exp(-value))  # Sigmoid function
+
+    @staticmethod
+    def __mean_squared_error(preds, targets) -> int or float:
+        length = len(preds)
+        preds = np.array(preds)
+        targets = np.array(targets)
+        squared_error = [(tar - val) ** 2 for tar, val in zip(targets, preds)]
+        mse = (np.sum(squared_error) / length)
+        return mse
+
+    def __backpropagation(self, entry_data, preds, targets, activations):
+            out_error = self.__mean_squared_error(preds, targets)
+            for i, activ in enumerate(activations):
+                layers_error = list()
+                r_activ = activ[::-1]  # Return the list reversed
+                out_delta = [out_error * self.__activation_function(y, deriv=True) for y in r_activ[0]]
+                layers_error.append(out_delta)
+                for index, layer in enumerate(reversed(self.weights)):
+                    transpose_weights = layer.T
+
+                    # How much each weight impacted at the error of each neuron
+                    hdl_error = [np.dot(d, transpose_weights).tolist() for d in layers_error[-1]]
+                    print(r_activ[index][0])
+                    print(hdl_error[0]*r_activ[index][0], end='\n\n\n')
+                    # Calculating the gradient of each neuron
+                    hdl_delta = [(i * self.__activation_function(y, deriv=True)) for i, y in zip(hdl_error, r_activ[index])]
+                    layers_error.append(hdl_delta)
+                    sleep(2)
+
+                gradients = layers_gradients[::-1]
+                for index in range(len(self.weights)):
+                    self.weights[index] -= self.eta * np.dot(activations[l])
 
     # Method that do the operations between the hidden layers
     def network_config(self, training_data, target, neurons_layers: list, epoch=10, batch_size=10, eta=0.01):
@@ -53,50 +75,46 @@ class NeuralNetwork(SGD):
         # Section to generate the weights to all layers
         preds = list()
         targets = list()
+        inp_tests = list()
+        activ_h_l = list()
         count = 0
         batches = [self.training_data[batch:batch + self.batch_size] for batch in
                    range(0, len(self.training_data), self.batch_size)]
+
         for index, data in enumerate(batches):
-            print(f'Treinamento Nº: {Colors.BG_RED}{index}{Colors.RESET}\n')
-            for test in data:
-                pred = self.__layers(test.tolist(), self.hidden_layers[1:], self.targets[count])
+            for entry in data:
+                print(f'Treinamento Nº: {Colors.BG_RED}{count}{Colors.RESET}\n')
+                (pred, activ_h) = self.__layers(entry, self.hidden_layers[1:], self.targets[count])
                 targets.append(self.targets[count])
                 preds.append(pred)
+                inp_tests.append(entry.tolist())
+                activ_h_l.append(activ_h)
                 count += 1
-            super().mean_squared_error(preds, targets)
+            self.__backpropagation(inp_tests, preds, targets, activ_h_l)
 
     def __layers(self, inp_data, hd_layers: list | int, target):
         # Array that will contain all the inputs of each layer
         input_matrix = list()
         input_matrix.append(inp_data)
-        neuron_count = 0
 
-        for index, neurons in enumerate(hd_layers):
-            layer_result = list()  # List containing the neurons output
-            for i in range(neurons):
-                result = self.__perceptron(input_matrix[index],
-                                           self.weights[index][i],
-                                           self.biases[index][i])  # Call the neuron method
-                sigmoid = self.__activation_function(result)  # Call the sigmoid function
+        #  Loop for each layer
+        for index, (weights, bias) in enumerate(zip(self.weights, self.biases)):
+            data = [input_matrix[index] for _ in range(len(weights))]
+            lp = [(np.dot(data[i], weights[i]) + bias[i])[0] for i in range(len(weights))]
+            r_sigmoid = [self.__activation_function(o) for o in lp]
+            l_result = [x if x > self.threshold else 0 for x in r_sigmoid]
+            input_matrix.append(l_result)
 
-                # The sigmoid returns must be greater than the threshold to activate
-                if sigmoid > self.threshold:
-                    layer_result.append(sigmoid)
-                else:
-                    layer_result.append(0)
-
-                neuron_count += 1
-            # Append the layer results at the input matrix
-            input_matrix.append(layer_result)
         out_array = input_matrix[-1]
         output = out_array.index(max(out_array))
 
+        #
         print(f'{Colors.PURPLE}[N]{Colors.RESET} >>', end=' ')
         for num, out in enumerate(out_array):
             print(f'{Colors.HARD_RED}[{num}]{Colors.RESET}: [{out:.2f}];', end=' ')
-        print(f'\n{Colors.YELLOW}[+]{Colors.RESET} Most Rated : {max(input_matrix[-1]):.2f} ', end='\n')
+        print(f'\n{Colors.YELLOW}[+]{Colors.RESET} Most Rated : {max(out_array):.2f} ', end='\n')
         i = 'C' if output == target else 'W'
         print(f'{Colors.CIAN}[*]{Colors.RESET} Resposta Correta >> {Colors.GREEN}{target}{Colors.RESET}', end='\n')
         print(f'{Colors.RED}[{i}]{Colors.RESET} Resposta RN >> {Colors.HARD_RED}{output}{Colors.RESET}',
               end='\n\n\n\n')
-        return output
+        return output, input_matrix[1:]
