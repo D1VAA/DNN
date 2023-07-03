fixed some wrong implementations of backpropagation

DNN.py
@@ -1,99 +1,108 @@
 import numpy as np
 from time import sleep
 from bcolors import Colors
+from random import shuffle
 
 
 class NeuralNetwork:
     def __init__(self):
-        self.hidden_layers = None
+        self.layers = None
         self.training_data = None
         self.targets = None
         self.biases = None
         self.weights = None
-        self.epoch = None
         self.batch_size = None
-        self.eta = None
         self.output = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.threshold = 0.6
-        self.results = list()
 
     @classmethod
     def __activation_function(cls, value: int, deriv=False) -> int or float:
         if deriv:
+            # Return the derivative of sigmoid function
             return cls.__activation_function(value) * (1 - cls.__activation_function(value))
         return 1 / (1 + np.exp(-value))  # Sigmoid function
 
     @staticmethod
     def __mean_squared_error(preds, targets) -> int or float:
-        # mse = np.square(np.subtract(targets, preds)).mean()
-        mse = (targets - preds) ** 2
-        deriv_mse = -2 * (targets - preds)
+        mse = np.mean((targets - preds) ** 2)
+        deriv_mse = -2 * (targets - preds) / preds
+        # Return the mse and the derivative of the error
         return mse, deriv_mse
 
-    def __backpropagation(self, preds, targets, activations, deriv_activ):
-        # Retorna o erro quadrático médio e a derivada do mesmo
-        out_error, deriv_out_error = self.__mean_squared_error(preds, targets)
+    def __backpropagation(self, deriv_mse, activations, deriv_activ):
+        r_activ = list(reversed(activations))[1:]  # Activation list reversed - Length : 3 (include input layer)
+        r_deriv_activ = list(
+            reversed(deriv_activ))  # Derivative activation list reversed - Length : 4 (include input layer)
+        r_biases = list(reversed(self.biases))  # Bias list reversed - Length : 3 (exclude input layer)
+        r_weights = list(reversed(self.weights))  # Weights list reversed - Length : 3 (only 3 connections)
 
-        # listas para organizar os resultados
-        layers_error = list()
-        c = 1
-        new_weights = list()
-        new_bias = list()
+        i_actived_neuron = activations[-1].index(max(activations[-1]))  # Activated neuron position
 
-        # Retorna a lista que contém as ativações de cada layer de forma invertida (da saída para a entrada)
-        r_activ = activations[::-1]
-        # O mesmo que a lista anterior, mas com as derivadas das funções de ativação
-        r_deriv_activ = deriv_activ[::-1]
-        r_biases = self.biases[::-1]  # Lista com os bias de cada camada invertida
-        weights = self.weights[::-1]  # Lista com os pesos mas invertida
+        for i, (weights, biases) in enumerate(zip(r_weights, r_biases)):
+            # Neurons local gradient
+            if i == 0:
+                neu_out_g_list = [d_a * deriv_mse if i == i_actived_neuron else 0 for i, d_a in
+                                  enumerate(r_deriv_activ[i])]
+                # Bias update
+                new_bias = np.array([x - y for x, y in zip(self.biases[-1 - i], neu_out_g_list)])
+                self.biases[-1 - i] = new_bias
 
-        # Get the position of the neuron that were activated on the output layer
-        i_actived_neuron = r_activ[0].index(max(r_activ[0]))
+                # Weight update
+                w_grad = np.array([np.array(x) * r_activ[i] for x in neu_out_g_list])
+                self.weights[-1 - i] -= (self.eta * w_grad)
+                propag = np.array([n * w for n, w in zip(neu_out_g_list, weights)])
+                new_neu_grad = np.array([x * y for x, y in zip(r_deriv_activ[1 + i], propag.T)])
+                neu_out_g_list = new_neu_grad[new_neu_grad != 0]
 
-        # Calc the gradient of the neuron activated in the output layer
-        out_grad = np.array(deriv_out_error * r_deriv_activ[0][i_actived_neuron])  # Derivative of e by z
-        f_up = out_grad * r_activ[1]
-        weight_update = self.weights[-1][i_actived_neuron] - (self.eta * f_up)
-        weights[0][i_actived_neuron] = weight_update
+            elif i == len(r_weights) - 1:
+                new_bias = np.array([x - y for x, y in zip(self.biases[-1 - i], neu_out_g_list)])
+                self.biases[-1 - i] = new_bias
 
-        propag = np.array(f_up * weights[0][i_actived_neuron]) * r_deriv_activ[1]
-        layers_error.append(propag)
+                w_grad = np.array([np.array(x) * r_activ[i] for x in neu_out_g_list])
+                self.weights[-1 - i] -= (self.eta * w_grad)
 
-        for i, layer in enumerate(weights[1:]):
-            w_grad = [x * r_activ[2 + i] for x in layers_error[-1]]
-            w_up = layer - (self.eta * w_grad)
-            weights[i + 1] = w_up
+            else:
+                w_grad = np.array([np.array(x) * r_activ[i] for x in neu_out_g_list])
+                self.weights[-1 - i] -= (self.eta * w_grad)
+                propag = np.array([n * w for n, w in zip(neu_out_g_list, weights)])
+                new_neu_grad = np.array([x * y for x, y in zip(r_deriv_activ[1 + i], propag.T)])
+                neu_out_g_list = new_neu_grad[new_neu_grad != 0]
 
-            # Updating variables
-            propag = np.dot(r_deriv_activ[1 + i], np.array(layers_error[-1] * layer))
-            print(propag)
-            layers_error.append(propag)
-            sleep(2)
+                new_bias = np.array([x - y for x, y in zip(self.biases[-1 - i], neu_out_g_list)])
+                self.biases[-1 - i] = new_bias
 
     # Method that do the operations between the hidden layers
-    def network_config(self, training_data, target, neurons_layers: list, epoch=10, eta=5):
-        self.training_data = training_data  # Array containg the input of each layer
-        self.epoch = epoch
-        self.eta = eta
+    def network_config(self, training_data, target, neurons_layers: list, epoch=10, eta=0.05):
+        self.training_data = np.array(training_data)  # Array containg the input of each layer
+        self.epoch = epoch  # Times to execute all the data training
+        self.eta = eta  # Learning rate
 
+        # Inserts the length of the input data at the list begining
         neurons_layers.insert(0, len(training_data.tolist()[0]))
+        # Inserts the length of the output data at the list end
         neurons_layers.append(len(self.output))
 
+        # Random normal gaussian variables with mean = 0 and standard deviation = 1/Vnin
         self.weights = [np.random.normal(0, 1 / np.sqrt(x), (y, x)) for x, y in
                         zip(neurons_layers[:-1], neurons_layers[1:])]
+        # Random normal gaussian variables with mean = 0 and standard deviation = 1
         self.biases = [np.random.randn(y, 1) for y in neurons_layers[1:]]
-        self.hidden_layers: list[int] = neurons_layers
+        self.layers: list[int] = neurons_layers
         self.targets = target
 
     def train(self):
-        # Section to generate the weights to all layers
         count = 0
         for e in range(self.epoch):
-            for data, target in zip(self.training_data, self.targets):
+            training_data = np.random.shuffle(self.training_data)
+            # For each data in all training data
+            for data, target in zip(training_data, self.targets):
                 print(f'Treinamento Nº: {Colors.BG_RED}{count}{Colors.RESET}\n')
-                (pred, activ_h, deriv_h) = self.__layers(data, self.hidden_layers[1:], target)
-                count += 1
-                self.weights = self.__backpropagation(pred, target, activ_h, deriv_h)
+                # Pred = output, activ_h: activations, deriv_h = derivative of each activation
+                (pred, activ_h, deriv_h) = self.__layers(data, self.layers[1:], target)
+                out_error, deriv_mse = self.__mean_squared_error(pred, target)
+                if out_error != 0:
+                    self.__backpropagation(deriv_mse, activ_h, deriv_h)  # Call the backpropagation method
+                count += 1  # To count the executions
 
     def __layers(self, inp_data, hd_layers: list | int, target):
         # Array that will contain all the inputs of each layer
@@ -112,10 +121,10 @@ class NeuralNetwork:
             input_matrix.append(l_result)
             deriv_sigmoid.append(d_sigmoid)
 
-        out_array = input_matrix[-1]
+        out_array = input_matrix[-1]  # Select the output layer
+        # Get the index of the activated neuron at the output layer
         output = out_array.index(max(out_array))
 
-        #
         print(f'{Colors.PURPLE}[N]{Colors.RESET} >>', end=' ')
         for num, out in enumerate(out_array):
             print(f'{Colors.HARD_RED}[{num}]{Colors.RESET}: [{out:.2f}];', end=' ')
@@ -124,4 +133,6 @@ class NeuralNetwork:
         print(f'{Colors.CIAN}[*]{Colors.RESET} Resposta Correta >> {Colors.GREEN}{target}{Colors.RESET}', end='\n')
         print(f'{Colors.RED}[{i}]{Colors.RESET} Resposta RN >> {Colors.HARD_RED}{output}{Colors.RESET}',
               end='\n\n\n\n')
+
+        # Return the output, the activations of each layer and the derivative of each activation
         return output, input_matrix, deriv_sigmoid